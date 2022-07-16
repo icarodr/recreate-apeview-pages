f2d59922c146b65e70d2f3e8e1642319f165b7a6
icarodr <it46009@gmail.com>
2022-07-16 02:44:58 -0300

    changes on index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './login-page';
+import App2 from './register-page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const root2 = ReactDOM.createRoot(document.getElementById('root2'));
 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
+root2.render(
+    <React.StrictMode>
+      <App2 />
+    </React.StrictMode>
+  );
